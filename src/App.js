65f017659948cf9586a042643c63164a9cbcef12
hind <hind-mansour@windowslive.com>
2024-01-31 18:36:41 +0100

    Adding project to github

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Welcome</h1>
+       <h2>Here we learn swedish the right way :D</h2>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
