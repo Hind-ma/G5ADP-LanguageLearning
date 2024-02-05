c069d26c897a28b0674a654b4d25a8924743a410
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2024-02-05 15:56:47 +0100

    Added all the different buttons needed for the different pages, and to logout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,20 @@
+import ChangePageButton from "./ChangePageButton";
+
 function Home() {
-  return <div>Home</div>;
+  return <div>Home
+    {/* Navigation Buttons */}
+      <div className="button-container">
+        <ChangePageButton to="/welcome" label="Go to Welcome" />
+        <ChangePageButton to="/" label="Go to Home" />
+        <ChangePageButton to="/account" label="Go to Account" />
+        <ChangePageButton to="/pick-word" label="Go to Pick-word" />
+        <ChangePageButton to="/introduce" label="Go to Introduce words" />
+        <ChangePageButton to="/fill-blank" label="Go to Fill in the blanks" />
+        <ChangePageButton to="/sentence" label="Go to Make a sentence" />
+        <ChangePageButton to="/connect-words" label="Go to Connect-words" />
+        <ChangePageButton to="/welcome" label="LOG OUT" />
+      </div>
+  </div>;
 }
 
 export default Home;
