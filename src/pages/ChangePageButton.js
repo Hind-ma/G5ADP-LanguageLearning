c3dcfcc27cb24a7664a33e042495a923be081e1d
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2024-02-05 15:55:57 +0100

    Created a modular component for a button that one can use to change the page

---
+++ src/pages/ChangePageButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const ChangePageButton = ({ to, label }) => {
+    const nav = useNavigate();
+
+    const changePage = () => {
+        nav(to);
+    };
+
+    return (
+        <button onClick={changePage}>
+            {label}
+        </button>
+    );
+};
+
+export default ChangePageButton;
