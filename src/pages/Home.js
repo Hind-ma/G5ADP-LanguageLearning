d85b9bdbd31a5f58b045129d010a37e8ca2b4e38
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2024-02-05 15:59:33 +0100

    Updated the labels for the buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,14 @@ function Home() {
   return <div>Home
     {/* Navigation Buttons */}
       <div className="button-container">
-        <ChangePageButton to="/welcome" label="Go to Welcome" />
-        <ChangePageButton to="/" label="Go to Home" />
-        <ChangePageButton to="/account" label="Go to Account" />
-        <ChangePageButton to="/pick-word" label="Go to Pick-word" />
-        <ChangePageButton to="/introduce" label="Go to Introduce words" />
-        <ChangePageButton to="/fill-blank" label="Go to Fill in the blanks" />
-        <ChangePageButton to="/sentence" label="Go to Make a sentence" />
-        <ChangePageButton to="/connect-words" label="Go to Connect-words" />
+        <ChangePageButton to="/welcome" label="Go to Welcome page" />
+        <ChangePageButton to="/" label="Go to Home page" />
+        <ChangePageButton to="/account" label="Go to Account page" />
+        <ChangePageButton to="/pick-word" label="Start session Pick a word" />
+        <ChangePageButton to="/introduce" label="Start session Introduce words" />
+        <ChangePageButton to="/fill-blank" label="Start session Fill in the blanks" />
+        <ChangePageButton to="/sentence" label="Start session Make a sentence" />
+        <ChangePageButton to="/connect-words" label="Start session Connect-words" />
         <ChangePageButton to="/welcome" label="LOG OUT" />
       </div>
   </div>;
