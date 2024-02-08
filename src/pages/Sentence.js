8379042ece375bf9f1c9d8be9e14b0073ed9949a
Chiranjibee Satapathy <chiranjibee12@gmail.com>
2024-02-08 01:53:28 +0100

    WIP ADG5-5
    + Modified util.js to add another function
    + Modified Sentence.js

--- src/pages/Sentence.js
+++ src/pages/Sentence.js
@@ -1,9 +1,64 @@
 import ChangePageButton from "./ChangePageButton";
+import { GetRandomInt, ShuffleArray } from "../utils";
+import { useState } from "react";
+
+/* TODO: @CS, Remove in future - this is only for Sprint 1 demo */
+const makeSentences = [
+    { id: 0, words: ["ursakta", "var", "kan", "jag", "hitta", "mjolken"] },
+    { id: 1, words: ["den", "ar", "halv", "sju"] },
+    { id: 2, words: ["tack", "for", "en", "rolig", "fest"] },
+    { id: 3, words: ["jag", "trivs", "bra", "i", "Sverige"] },
+    { id: 4, words: ["Lisa", "laser", "svenska", "pa", "universitetet"] },
+]
+
+/* TODO: @CS,give proper function name, add space between buttons */
+function DisplayShuffledSentence({ id }) {
+    const item = makeSentences.find(sen => sen.id === id);
+    var shufflewords = item.words;
+    shufflewords = ShuffleArray(shufflewords);
+
+    // TODO: @CS, add the custom button
+    const sen = shufflewords.map(word =>
+        <button>{word.toString()}</button>
+    );
+
+    return (
+        <div>
+            {/* id is for debugging */}
+            {/* {item.id} */}  
+            <p>{sen}</p>
+        </div>
+    )
+}
+
+function NextSentenceButton({ onClick }) {
+    return (
+        <div>
+            <button onClick={onClick}>
+                Next Sentence
+            </button>
+        </div>
+    );
+}
 
 function Sentence() {
-  return <div>Make a sentence
-    <ChangePageButton to="/" label="Go to Home" />
-  </div>;
+    const startIdx = GetRandomInt(0, makeSentences.length - 1);
+    const [currentIdx, setCurrentIdx] = useState(startIdx);
+
+    function updateMakeSentence() {
+        setCurrentIdx(currentIdx => (currentIdx + 1 > makeSentences.length - 1) ? 0 : currentIdx + 1);
+    }
+
+    return (
+        <div>
+            <ChangePageButton to="/" label="Go to Home" />
+            <div className="intro-word">
+                <h2>MAKE THE SENTENCE</h2>
+                <DisplayShuffledSentence id={currentIdx} />
+                <NextSentenceButton onClick={updateMakeSentence} />
+            </div>
+        </div>
+    );
 }
 
 export default Sentence;
--- src/utils.js
+++ src/utils.js
@@ -11,3 +11,27 @@ export function GetRandomInt(min, max) {
     const maxFloor = Math.floor(max);
     return Math.floor(Math.random() * (maxFloor - minCeil + 1) + minCeil);
 }
+
+/**
+ * Shuffle Array - unbiased shuffle algorithm
+ * Link: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ * @param {any} array - input array
+ * @returns - shuffled array
+ */
+export function ShuffleArray(array) {
+    let currentIdx = array.length;
+    let randomIdx = null;
+
+    // While there are remaining elements to shuffle
+    while (currentIdx > 0) {
+
+        // Pick the remaining element
+        randomIdx = Math.floor(Math.random() * currentIdx);
+        currentIdx--;
+
+        // Swap with the current element
+        [array[currentIdx], array[randomIdx]] = [array[randomIdx], array[currentIdx]];
+    }
+
+    return array;
+}
