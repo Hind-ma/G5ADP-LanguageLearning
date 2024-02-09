a813e65fca39af1eb11c17095fcc7ed61f5dca60
puerto <puerto@student.chalmers.se>
2024-02-09 18:45:14 +0100

    The main functionality of the sentence feature is implemented

--- src/pages/Sentence.js
+++ src/pages/Sentence.js
@@ -1,19 +1,29 @@
 import ChangePageButton from "./ChangePageButton";
 import React, {useState} from "react"
+import { GetRandomInt } from "../utils";
 
 const sentenceList = [
   {s:"Goddag, vad heter du?", e: "Good day, what is your name?" },
   {s:"Vet du var centralstationen ligger?", e: "Do you know where the central station is?" },
   {s:"Åh förlåt mig, jag äter inte kött.", e: "Oh I'm sorry, I don't eat meat." },
-  {s:"Sista bussen går om tre minuter.", e: "The last bus leaves in three minutes" },
+  {s:"Sista bussen går om tre minuter.", e: "The last bus leaves in three minutes." },
   {s:"Jag har bara 100 kronor kvar.", e: "I only have 100 kronor left." },
 ];
 
 function SetSentences({pos}) {
   return (
-    <div>
-      <p>{sentenceList[pos].s}</p>
-    </div>
+    <p>{sentenceList[pos].s}</p>
+  );
+}
+
+function NextButton ({pos, movePos}) {
+  if (pos === sentenceList.length-1) {
+    return (
+    <ChangePageButton to="/" label="Finished! Go back to the home page"/>
+    );
+  }
+  return (
+    <button id="next" onClick={movePos}>Next sentence</button>
   );
 }
 
@@ -22,11 +32,8 @@ function Sentence() {
   const [position, setPosition] = useState(0);
 
   const movePosition = () => {
-    console.log(position);
     if (position < sentenceList.length -1) {
       setPosition(position+1);
-    } else {
-      setPosition(0);
     }
     setSentence("");
     document.getElementById("result").innerHTML = "";
@@ -34,8 +41,7 @@ function Sentence() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    var correctSentence = sentenceList[position].e;
-    if (sentence === correctSentence) {
+    if (sentence === sentenceList[position].e) {
       document.getElementById("result").innerHTML = "Sentence correctly translated!";
     } else {
       document.getElementById("result").innerHTML = "Incorrect translation.";
@@ -46,8 +52,8 @@ function Sentence() {
     <ChangePageButton to="/" label="Go to Home" />
     <h2>Translate the swedish sentence into english.</h2>
     <div className="auth-form-container">
-      <SetSentences pos = {position}/>
-      <button onClick={movePosition}>Next sentence</button>
+      <SetSentences pos={position}/>
+      <NextButton pos = {position} movePos = {movePosition}/>
       <form className="login-form" onSubmit={handleSubmit}>
                 <input value={sentence} onChange={(e) => setSentence(e.target.value)} type="name" placeholder="Type here" id="sentence" name="sentence"/>
                 
