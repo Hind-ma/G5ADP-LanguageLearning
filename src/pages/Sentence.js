46b0b938b3a1d3bbab5a58868f65ec877debcdaf
puerto <puerto@student.chalmers.se>
2024-02-10 14:31:23 +0100

    Added randomization to the language of the sentences

--- src/pages/Sentence.js
+++ src/pages/Sentence.js
@@ -3,16 +3,16 @@ import React, {useState} from "react"
 import { GetRandomInt } from "../utils";
 
 const sentenceList = [
-  {s:"Goddag, vad heter du?", e: "Good day, what is your name?" },
-  {s:"Vet du var centralstationen ligger?", e: "Do you know where the central station is?" },
-  {s:"Åh förlåt mig, jag äter inte kött.", e: "Oh I'm sorry, I don't eat meat." },
-  {s:"Sista bussen går om tre minuter.", e: "The last bus leaves in three minutes." },
-  {s:"Jag har bara 100 kronor kvar.", e: "I only have 100 kronor left." },
+  {first:"Goddag, vad heter du?", second: "Good day, what is your name?" },
+  {first:"Vet du var centralstationen ligger?", second: "Do you know where the central station is?" },
+  {first:"Åh förlåt mig, jag äter inte kött.", second: "Oh I'm sorry, I don't eat meat." },
+  {first:"Sista bussen går om tre minuter.", second: "The last bus leaves in three minutes." },
+  {first:"Jag har bara 100 kronor kvar.", second: "I only have 100 kronor left." },
 ];
 
 function SetSentences({pos}) {
   return (
-    <p>{sentenceList[pos].s}</p>
+    <p>{sentenceList[pos].first}</p>
   );
 }
 
@@ -27,9 +27,24 @@ function NextButton ({pos, movePos}) {
   );
 }
 
+function RandomizeLanguage () {
+  for (let i = 0; i < sentenceList.length; i++) {
+    var lan = GetRandomInt(0,1);
+    if (lan === 1) {
+    [sentenceList[i].first, sentenceList[i].second] = [sentenceList[i].second, sentenceList[i].first];
+    }
+  }
+}
+
 function Sentence() {
   const [sentence, setSentence] = useState("");
   const [position, setPosition] = useState(0);
+  const [randomize, setRandomized] = useState(false);
+  
+  if (!randomize) {
+    RandomizeLanguage();
+    setRandomized(true);
+  }
 
   const movePosition = () => {
     if (position < sentenceList.length -1) {
@@ -41,7 +56,7 @@ function Sentence() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (sentence === sentenceList[position].e) {
+    if (sentence === sentenceList[position].second) {
       document.getElementById("result").innerHTML = "Sentence correctly translated!";
     } else {
       document.getElementById("result").innerHTML = "Incorrect translation.";
