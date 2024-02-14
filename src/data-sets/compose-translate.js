ca253d98f3ddf048873576b6c285260b25de8cbd
puerto <puerto@student.chalmers.se>
2024-02-14 12:41:58 +0100

    Created the dataset for the compose-translate features with 20 sentences

---
+++ src/data-sets/compose-translate.js
@@ -0,0 +1,22 @@
+const completeList = [
+  {id: 0, swedish:"Goddag, vad heter du?", words:["goddag", "vad", "heter", "du"], english: "Good day, what is your name?" },
+  {id: 1, swedish:"Vet du var centralstationen ligger?", words:["vet", "du", "var", "centralstationen", "ligger"], english: "Do you know where the central station is?" },
+  {id: 2, swedish:"Åh förlåt mig, jag äter inte kött.", words:["åh", "förlåt", "mig", "jag", "äter", "inte", "kött"], english: "Oh I'm sorry, I don't eat meat." },
+  {id: 3, swedish:"Sista bussen går om tre minuter.", words:["sista", "bussen", "går", "om", "tre", "minuter"], english: "The last bus leaves in three minutes." },
+  {id: 4, swedish:"Jag har bara 100 kronor kvar.", words:["jag", "har", "bara", "100", "kronor", "kvar"], english: "I only have 100 kronor left." },
+  {id: 5, swedish:"Ursäkta, var kan jag hitta mjölken?", words: ["ursäkta", "var", "kan", "jag", "hitta", "mjölken"], english: "Excuse me, where can I find the milk?" },
+  {id: 6, swedish:"Den är halv sju.", words: ["den", "är", "halv", "sju"], english: "It's six thirty." },
+  {id: 7, swedish:"Tack för en rolig fest.", words: ["tack", "för", "en", "rolig", "fest"], english: "Thanks for a fun party." },
+  {id: 8, swedish:"Jag trivs bra i Sverige.", words: ["jag", "trivs", "bra", "i", "Sverige"], english: "I enjoy being in Sweden." },
+  {id: 9, swedish:"Lisa läser svenska på universitetet.", words: ["Lisa", "läser", "svenska", "på", "universitetet"], english: "Lisa studies swedish at the university." },
+  {id: 10, swedish:"Hur många datorer finns att låna?", words: ["hur", "många", "datorer", "finns", "att", "låna"], english: "How many computers are available to borrow?" },
+  {id: 11, swedish:"Akta dig, det är väldigt halt.", words: ["akta", "dig", "det", "är", "väldigt", "halt"], english: "Watch out, it's very slippery" },
+  {id: 12, swedish:"Du måste skynda på dig.", words: ["du", "måste", "skynda", "på", "dig"], english: "You have to hurry up." },
+  {id: 13, swedish:"Ge mig kökskniven.", words: ["ge", "mig", "kökskniven"], english: "Give me the kitchen knife." },
+  {id: 14, swedish:"Hur mycket kostar en enkelbiljett?", words: ["hur", "mycket", "kostar", "en", "enkelbiljett"], english: "How much does a single ticket cost?" },
+  {id: 15, swedish:"Kan man byta till färjan på nästa stationen?", words: ["kan", "man", "byta", "till", "färjan", "på", "nästa", "stationen"], english: "Can you take the ferry in the next station?" },
+  {id: 16, swedish:"Får man parkera här?", words: ["får", "man", "parkera", "här"], english: "Can you park here?" },
+  {id: 17, swedish:"Kan du visa din legitimiation, tack?", words: ["kan", "du", "visa", "din", "legitimation", "tack"], english: "Can you show your ID please?" },
+  {id: 18, swedish:"Hur mycket kostar en enkelbiljett?", words: ["hur", "mycket", "kostar", "en", "enkelbiljett"], english: "How much does a single ticket cost?" },
+  {id: 19, swedish:"Vad kan jag köpa med 100 kronor?", words: ["vad", "kan", "jag", "köpa", "med", "100", "kronor"], english: "What can I buy with 100 kronor?" }
+]
