8d6ba56ad8fac084330d4439464d768b13802ddc
Chiranjibee Satapathy <chiranjibee12@gmail.com>
2024-02-21 21:20:05 +0100

    Page Routing Fix
    + Fixed the page routing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,8 @@ function Home() {
     {/* Navigation Buttons */}
       <h4>Welcome: {currentUsername}</h4>
       <div className="button-container">
-        <ChangePageButton to="/welcome" label="Go to Welcome page" />
-        <ChangePageButton to="/" label="Go to Home page" />
+        <ChangePageButton to="/" label="Go to Welcome page" />
+        <ChangePageButton to="/home" label="Go to Home page" />
         <ChangePageButton to="/account" label="Go to Account page" />
         <ChangePageButton to="/pick-word" label="Start session Pick a word" />
         <ChangePageButton to="/introduce" label="Start session Introduce words" />
@@ -16,7 +16,7 @@ function Home() {
         <ChangePageButton to="/TranslateSentence" label="Start session Translate a sentence" />
         <ChangePageButton to="/create-sen" label="Start session Create a sentence" />
         <ChangePageButton to="/connect-words" label="Start session Connect-words" />
-        <ChangePageButton to="/welcome" label="LOG OUT" />
+        <ChangePageButton to="/" label="LOG OUT" />
       </div>
   </div>;
 }
