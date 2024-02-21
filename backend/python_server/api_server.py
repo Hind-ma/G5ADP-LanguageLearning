8fea29c5bac682f8a62e470e8389c744809f83b0
KristofferGus <gusguskrp@student.gu.se>
2024-02-21 19:55:14 +0100

    Hooked up handeler to api

--- backend/python_server/api_server.py
+++ backend/python_server/api_server.py
@@ -1,10 +1,11 @@
 from flask import Flask, request
 import sys
+from swegptmodel import swegpt_handeler
 
 app = Flask(__name__)
 
 
-
+swe_gbt = swegpt_handeler()
 
 @app.route('/greet')
 def greet():
@@ -12,6 +13,18 @@ def greet():
     name = request.args.get('name', 'Anonymous')
     greeting = request.args.get('greeting', 'Hello')
     return f'{greeting}, {name}!'
+
+@app.route('/get_fill_in_prob')
+def get_fill_in_prob():
+    before = request.args.get('before', '')
+    fill = request.args.get('fill', '')
+    after = request.args.get('after', '')
+    ratio = request.args.get('ratio', '')
+    if ratio != "":
+        prob = swe_gbt.calc_fill_prob(before, fill, after,  float(ratio))
+    else:
+        prob = swe_gbt.calc_fill_prob(before, fill, after)
+    return str(prob)
 
 
 if __name__ == '__main__':
