688f5c0e3092a239c493d53aa9ba9a83776f3c97
stihan01 <stihan01@gmail.com>
2024-02-28 22:59:11 +0100

    fixed next button coloring

--- src/data-sets/fillBlank.js
+++ src/data-sets/fillBlank.js
@@ -1,29 +1,74 @@
 export const sentenceList = [
-    {id:0, sentence:"Vi åker _ till jobbet", answer:"tåget"},
-    {id:1, sentence:"Mitt favorit ämne i skolan är _, jag gillar ekvationer", answer:"matte"},
-    {id:2, sentence:"Jag brukar åka till _ varannan vecka för att låna böcker", answer:"biblioteket"},
-    {id:3, sentence:"Är du i _ ? kan jag snälla få ett glas vatten", answer:"köket"},
-    {id:4, sentence:"Min pappa är _, han jobbar på sjukhust ", answer:"läkare"},
-    {id:5, sentence:"Min syster är lärare,hon jobbar på _", answer:"skolan"},
-    {id:6, sentence:"Ska vi gå kolla den nya _ på bio? ", answer:"filmen"},
-    {id:7, sentence:"Jag gillar _ mat, därför använder jag mycket svartpeppar ", answer:"stark"},
-    {id:8, sentence:"Min favorit _ är glass ", answer:"efterrätt"},
-    {id:9, sentence:"Kan jag få låna din _ min mobil har bara 10% ", answer:"laddare"},
-    {id:10, sentence:"Gillar du _ eller te? ", answer:"kaffe"},
-    {id:11, sentence:"Vad gillar du för sorts _? Jag gillar komedi ", answer:"svår"},
-    {id:12, sentence:"I vilken _ bor du? Jag bor i Götborg ", answer:"stad"},
-    {id:13, sentence:"Vad gillar du för _? Jag gillar jazz ", answer:"musik"},
-    {id:14, sentence:"Hur många _ har du? Jag har en bro och en syster ", answer:"syskon"},
-    {id:15, sentence:"Vilka _ pratar du? Jag pratar svenska och engeleska ", answer:"språk"},
-    {id:16, sentence:"Ska vi öppna _? Det är väldigt varmt här ", answer:"fönstret"},
-    {id:17, sentence:"Här är mitt telefonnummer, du kan _ om du behöver hjälp ", answer:"ringa"},
-    {id:18, sentence:"Vad är ditt favorit _? Min är häst", answer:"djur"},
-    {id:19, sentence:"Hur många _ är den nya boken? Den gamla var 271 ", answer:"sidor"}
-]
-
-
-
-
-
-
-
+  { id: 0, sentence: "Vi _ tåget till jobbet", answer: "åker" },
+  {
+    id: 1,
+    sentence: "Mitt favorit ämne i skolan är _, jag gillar ekvationer",
+    answer: "matte",
+  },
+  {
+    id: 2,
+    sentence: "Jag brukar åka till _ varannan vecka för att låna böcker",
+    answer: "biblioteket",
+  },
+  {
+    id: 3,
+    sentence: "Tentan var _, jag kunde bara svara på en fråga",
+    answer: "svår",
+  },
+  {
+    id: 4,
+    sentence: "Min pappa är _, han jobbar på sjukhust ",
+    answer: "läkare",
+  },
+  { id: 5, sentence: "Min syster är lärare,hon jobbar på _", answer: "skolan" },
+  { id: 6, sentence: "Ska vi gå kolla den nya _ på bio? ", answer: "filmen" },
+  {
+    id: 7,
+    sentence: "Jag gillar _ mat, därför använder jag mycket svartpeppar ",
+    answer: "stark",
+  },
+  { id: 8, sentence: "Min favorit _ är glass ", answer: "efterrätt" },
+  {
+    id: 9,
+    sentence: "Kan jag få låna din _ min mobil har bara 10% ",
+    answer: "laddare",
+  },
+  { id: 10, sentence: "Gillar du _ eller te? ", answer: "kaffe" },
+  {
+    id: 11,
+    sentence: "Vad gillar du för sorts _? Jag gillar komedi ",
+    answer: "film",
+  },
+  { id: 12, sentence: "I vilken _ bor du? Jag bor i Götborg ", answer: "stad" },
+  {
+    id: 13,
+    sentence: "Vad gillar du för _? Jag gillar jazz ",
+    answer: "musik",
+  },
+  {
+    id: 14,
+    sentence: "Hur många _ har du? Jag har en bror och en syster ",
+    answer: "syskon",
+  },
+  {
+    id: 15,
+    sentence: "Vilka _ pratar du? Jag pratar svenska och engeleska ",
+    answer: "språk",
+  },
+  {
+    id: 16,
+    sentence: "Ska vi öppna _? Det är väldigt varmt här ",
+    answer: "fönstret",
+  },
+  {
+    id: 17,
+    sentence: "Här är mitt telefonnummer, du kan _ om du behöver hjälp ",
+    answer: "ringa",
+  },
+  { id: 18, sentence: "Vad är ditt favorit _? Min är häst", answer: "djur" },
+  {
+    id: 19,
+    sentence: "Hur många _ är den nya boken? Den gamla var 271 ",
+    answer: "sidor",
+  },
+];
