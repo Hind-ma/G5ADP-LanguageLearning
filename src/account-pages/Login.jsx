4ed990d1a4e2850aa8fe1de87c263735f53784c2
Aleksandra Likus <84852856+AleksandraLks@users.noreply.github.com>
2024-03-05 15:31:02 +0100

    Register.jsx and Login.jsx now have Account.css file

--- src/account-pages/Login.jsx
+++ src/account-pages/Login.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
+import "./Account.css";
 
 export const Login = (props) => {
     const [name, setName] = useState("");
@@ -8,36 +9,59 @@ export const Login = (props) => {
         e.preventDefault();
         const userData = sessionStorage.getItem(name);
         if (userData !== null) {
-            const userPassword = JSON.parse(userData)['password'];
+            const userPassword = JSON.parse(userData)["password"];
             if (userPassword === pass) {
-                sessionStorage.setItem('username', name);
-                document.getElementById("confirmation").innerHTML = "Login succesful";
-                document.getElementById('switchRegister').disabled = true;
-                document.getElementById('lCreate').disabled = true;
-                setTimeout(() => {props.continueToMenu(false);}, 1000);
+                sessionStorage.setItem("username", name);
+                document.getElementById("confirmation").innerHTML = "Login successful";
+                document.getElementById("switchRegister").disabled = true;
+                document.getElementById("lCreate").disabled = true;
+                setTimeout(() => {
+                    props.continueToMenu(false);
+                }, 1000);
             } else {
                 document.getElementById("confirmation").innerHTML = "Incorrect password";
             }
-            
         } else {
             document.getElementById("confirmation").innerHTML = "Username does not exist";
         }
-    }
-
+    };
 
     return (
         <div className="auth-form-container">
             <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="name">Name</label>
-                <input value={name} onChange={(e) => setName(e.target.value)} type="name" placeholder="Karl" id="name" name="name"/>
-                
-                <label htmlFor="password">Password</label>
-                <input value={pass}  onChange={(e) => setPass(e.target.value)} type="password" placeholder="*********" id="password" name="password"/>
-                
-                <button id="lCreate" type="submit">Log in</button>
+                <label className="font" htmlFor="name">Name</label>
+                <input
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    type="name"
+                    placeholder="Karl"
+                    id="name"
+                    name="name"
+                />
+
+                <label className="font" htmlFor="password">Password</label>
+                <input
+                    value={pass}
+                    onChange={(e) => setPass(e.target.value)}
+                    type="password"
+                    placeholder="*********"
+                    id="password"
+                    name="password"
+                />
+
+                <button id="lCreate" type="submit" className="next-button">
+                    Log in
+                </button>
                 <p id="confirmation"></p>
             </form>
-            <button id="switchRegister" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here</button>
+            <button
+                id="switchRegister"
+                onClick={() => props.onFormSwitch("register")}
+                className="check-button"
+            >
+                Don't have an account? Register here
+            </button>
         </div>
-    )
-}
+    );
+};
+
--- src/account-pages/Register.jsx
+++ src/account-pages/Register.jsx
@@ -1,61 +1,98 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
+import "./Account.css";
 
 export const Register = (props) => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [pass, setPass] = useState("");
-    const [confPass, setConfPass] = useState("");
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if ([name,email,pass,confPass].includes("")) {
-            document.getElementById("confirmation").innerHTML ="Some fields are empty";
-        } else {
-            var passwordsMatch = confPass === pass;
-            var nameAvailable = sessionStorage.getItem(name) === null;
-            if (nameAvailable) {
-                if (passwordsMatch) {
-                    storeData();
-                    sessionStorage.setItem('username', name);
-                    document.getElementById("confirmation").innerHTML = "The account has been created";
-                    document.getElementById('switchLogin').disabled = true;
-                    document.getElementById('create').disabled = true;
-                    setTimeout(() => {props.continueToMenu(false);}, 1000);
-                } else
-                    document.getElementById("confirmation").innerHTML = "Passwords do not match";
-            }
-            else {
-                document.getElementById("confirmation").innerHTML = "Username already taken";
-            }
-        }
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [confPass, setConfPass] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if ([name, email, pass, confPass].includes("")) {
+      document.getElementById("confirmation").innerHTML = "Some fields are empty";
+    } else {
+      var passwordsMatch = confPass === pass;
+      var nameAvailable = sessionStorage.getItem(name) === null;
+      if (nameAvailable) {
+        if (passwordsMatch) {
+          storeData();
+          sessionStorage.setItem("username", name);
+          document.getElementById("confirmation").innerHTML = "The account has been created";
+          document.getElementById("switchLogin").disabled = true;
+          document.getElementById("create").disabled = true;
+          setTimeout(() => {
+            props.continueToMenu(false);
+          }, 1000);
+        } else document.getElementById("confirmation").innerHTML = "Passwords do not match";
+      } else {
+        document.getElementById("confirmation").innerHTML = "Username already taken";
+      }
     }
+  };
 
-    const storeData = () => {
-        const userData = {'name' : name, 'email' : email, 'password' : pass};
-        const parsedUserData = JSON.stringify(userData);
-        sessionStorage.setItem(name, parsedUserData);
-    }
+  const storeData = () => {
+    const userData = { name: name, email: email, password: pass };
+    const parsedUserData = JSON.stringify(userData);
+    sessionStorage.setItem(name, parsedUserData);
+  };
 
-    return (
-        <div className="auth-form-container">
-            <form noValidate className="register-form" onSubmit={handleSubmit}>
-                <label htmlFor="name">Username</label>
-                <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Karl Nils" id="name" name="name"/>
+  return (
+    <div className="auth-form-container">
+      <form noValidate className="register-form" onSubmit={handleSubmit}>
+       <label className="font"  htmlFor="name">Name</label>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Karl Nils"
+          id="name"
+          name="name"
+        />
 
-                <label htmlFor="email">Email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Learn@swedish.com" id="email" name="email"/>
-                
-                <label htmlFor="password">Password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*****" id="password" name="password"/>
+       <label className="font" htmlFor="email">Email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Learn@swedish.com"
+          id="email"
+          name="email"
+        />
 
-                <label htmlFor="confirmPassword">Confirm password</label>
-                <input value={confPass}  onChange={(e) => setConfPass(e.target.value)} type="password" placeholder="*****" id="confirmPassword" name="confirmPassword"/>
-                
-                <button id="create" type="submit">Create account</button>
+       <label className="font" htmlFor="password">Password</label>
+        <input
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          type="password"
+          placeholder="*****"
+          id="password"
+          name="password"
+        />
 
-                <p id="confirmation"></p>
-            </form>
-            <button id="switchLogin" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here</button>
+       <label className="font" htmlFor="confirmPassword">Confirm password</label>
+        <input
+          value={confPass}
+          onChange={(e) => setConfPass(e.target.value)}
+          type="password"
+          placeholder="*****"
+          id="confirmPassword"
+          name="confirmPassword"
+        />
+
+        <button id="create" type="submit" className="button next-button">
+          Sign up
+        </button>
+
+        <p id="confirmation"></p>
+
+        <div className="login-link-container">
+          <p>Already have an account?</p>
+          <button id="switchLogin" onClick={() => props.onFormSwitch("login")} className="button check-button">
+            Log in
+          </button>
         </div>
-    )
-}
+      </form>
+    </div>
+  );
+};
